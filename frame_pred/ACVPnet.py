Fixed bug in average pixel calculations, save and load average pixel values, print periodically when loading training data, stop reading data after reaching 500,000 examples, calculate average pixel values with 500,000 examples

frame_pred/ACVPnet.py
@@ -179,16 +179,25 @@ def preprocessImages(images, avgs):
 def calcAvgPixel(head_directory):
     # Given list filenames, calculate average pixel values for each of the 3 channels
     pixel_sums = np.zeros((3))
-    num_pixels = 0
+    num_images = 0
     for directory, subdirs, files in os.walk(head_directory):
         if "actions.txt" not in files:
             continue
         files.remove("actions.txt")
+        enough = False
         for file in files:
             image = cv2.imread(directory + '/' + file)
             pixel_sums += image.sum(axis=(0, 1))
-            num_pixels += image.shape[0] * image.shape[1] * image.shape[2]
-    avg_pixels = pixel_sums / float(num_pixels)
+            num_images += 1
+            if num_images % 10000 == 0:
+                print "Calculated average pixel values for", num_images, "images"
+            if num_images >= 500000:
+                enough = True
+                break
+        if enough:
+            break
+
+    avg_pixels = pixel_sums / float(num_pixels*160*210)
     return avg_pixels
 
 def fileNum2FileName(num):
@@ -214,6 +223,7 @@ def readFilenamesAndActions(head_directory):
             print "Skipping episode", directory, "because it doesn't contain enough data."
             continue
         skip_ep = False
+        enough_data = False
         for i in range(len(action_tuples)):
             if i > 2 and i < len(action_tuples) - 5:
                 frames = [fileNum2FileName(action_tuples[i-3][1]), fileNum2FileName(action_tuples[i-2][1]), \
@@ -233,7 +243,14 @@ def readFilenamesAndActions(head_directory):
                 frames_actions_nextframes.append(([os.path.abspath(directory + '/' + f) for f in frames], actions, \
                     [os.path.abspath(directory + '/' + f) for f in next_frames]))
                 img_tot += 1
+                if img_tot % 10000:
+                    print "Read in filenames and actions for", img_tot, "training examples"
+                if img_tot >= 500000:
+                    enough_data = True
+                    break
         if skip_ep:
             continue
+        if enough_data:
+            break
     print "Read in a total of", img_tot, "training examples."
     return frames_actions_nextframes

frame_pred/train_net.py
@@ -17,11 +17,13 @@ from tensorpack.utils.gpu import get_nr_gpu
 
 from ACVPnet import *
 
-
 def main():
     parser = argparse.ArgumentParser(description="Train the ACVP image generation network.")
-    parser.add_argument("--episode_dir", type=str, default="/home/matt/acvp/frame_pred/fake_data", \
+    parser.add_argument("--episode_dir", type=str, default="/data/people/mcorsaro/acvp_03epsil/", \
         help="Directory containing episode subdirectories. NOTE: It must be the absolute path.")
+    parser.add_argument("--avg_pixel_file", type=str, default="", \
+        help="Directory containing average pixel file for a given dataset." + \
+        " If unspecified, a new file will be created. NOTE: It must be the absolute path.")
     parser.add_argument("--network", choices=["cnn", "rnn", "naff"], default="cnn",
         help="types of models are cnn, rnn, and naff")
 
@@ -32,9 +34,13 @@ def main():
     batch_size_cnn_naff = 32
     batch_size_rnn = 4
 
-    avgs = calcAvgPixel(args.episode_dir)
-    print "Average pixel values:", avgs, "TODO(MATT): Save these in a file, load at runtime"
     items = readFilenamesAndActions(args.episode_dir)
+    avgs = calcAvgPixel(args.episode_dir) if args.avg_pixel_file == "" else \
+        np.loadtxt(args.avg_pixel_file)
+    if args.avg_pixel_file == "":
+        # Save locally
+        np.savetxt(timestamp() + "_avg_pixels.txt", avgs)
+    print "Average pixel values:", avgs
 
     dataflow = AtariReplayDataflow(items, args.network, avgs, shuffle=True, \
         batch_size=(batch_size_rnn if args.network == "rnn" else batch_size_cnn_naff))